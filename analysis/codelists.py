96eab3a0695fb61419c71e3254a013d68fb37cca
acagreen17 <acagreen17@gmail.com>
2021-06-16 21:28:48 +0100

    codelists

---
+++ analysis/codelists.py
@@ -0,0 +1,51 @@
+######################################
+
+# Some covariates used in the study are created from codelists of clinical conditions or 
+# numerical values available on a patient's records.
+# This script fetches all of the codelists identified in codelists.txt from OpenCodelists.
+
+######################################
+
+
+# --- IMPORT STATEMENTS ---
+
+## Import code building blocks from cohort extractor package
+from cohortextractor import (codelist, codelist_from_csv, combine_codelists)
+
+
+# --- CODELISTS ---
+
+## Delayed prescriptions for antibiotics
+delayed_antibiotics_prescriptions_codes = codelist_from_csv(
+  "codelists/user-MillieGreen-delayed-prescriptions-for-antibiotics.csv",
+  system = "snomed",
+  column = "code",
+)
+
+## Record of a coded infection clinical event on same day as an antibiotic
+infection_codes = codelist_from_csv(
+  "codelists/user-MillieGreen-infection-tmp.csv",
+  system = "snomed",
+  column = "code",
+)
+
+## Patients in long-stay nursing and residential care
+carehome_primis_codes = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-longres.csv",
+  system = "snomed",
+  column = "code",
+)
+
+### Learning disabilities
+learning_disability_codes = codelist_from_csv(
+  "codelists/nhsd-primary-care-domain-refsets-ld_cod.csv",
+  system = "snomed",
+  column = "code",
+)
+
+### Serious Mental Illness
+serious_mental_illness_codes = codelist_from_csv(
+  "codelists/nhsd-primary-care-domain-refsets-mh_cod.csv",
+  system = "snomed",
+  column = "code",
+)
